6bc63020cad06e024c6e5e68202c6f2841cce7e4
SarKaa <51273849+Watking01@users.noreply.github.com>
2020-05-06 10:32:53 +0100

    Added iterm for windows

---
+++ iterm.bat
@@ -0,0 +1,8 @@
+@echo off
+Title Iphone terminal access
+set /P i= what is your iphone's ip address? 
+set /P c= sftp or ssh? 
+goto :ssh
+:ssh
+echo type in your root password (default is alpine)
+wsl -e %c% root@%i%
